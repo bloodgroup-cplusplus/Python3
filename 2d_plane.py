50a9c2bb5e149c74f74b6b62ef8c65365b3f50e5
cl-rs <81256027+cl-rs@users.noreply.github.com>
2021-10-12 22:04:30 +0530

    Update 2d_plane.py

--- 2d_plane.py
+++ 2d_plane.py
@@ -7,5 +7,6 @@ class TwoD(Scene):
 		origin_text = Text('(0,0)').next_to(dot,DOWN)
 		tip_text = Text('(2,2)').next_to(arrow.get_end(),RIGHT)
 		self.add(numberplane,dot, arrow, origin_text,tip_text)
+		#works only in manim and not in manimlib
 		
 		
