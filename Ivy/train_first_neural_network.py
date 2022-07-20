a135f79d12a67b0c7814487e625fc3335c3ad6c2
Bhushan <tucater10896601@protonmail.com>
2022-07-20 18:32:19 +0530

    replace the function to set_framework () and replace the source code with ivy.torch.reduce_mean()

--- Ivy/train_first_neural_network.py
+++ Ivy/train_first_neural_network.py
@@ -1,42 +1,28 @@
 import ivy
 
-
-class MyModel( ivy.Module):
+class MyModel(ivy.Module):
     def __init__(self):
-        self.linear0 = ivy.Linear(3,64)
-        self.linear1 = ivy.Linear(64,1)
+        self.linear0 = ivy.Linear(3, 64)
+        self.linear1 = ivy.Linear(64, 1)
         ivy.Module.__init__(self)
 
+    def _forward(self, x):
+        x = ivy.relu(self.linear0(x))
+        return ivy.sigmoid(self.linear1(x))
 
-
-        def forward(self,x):
-            x = ivy.relu(self.linear0(x))
-            return ivy.sigmoid(self.lienar1(x))
-
-
-
-ivy.set_backend('torch') # change to any backend 
+ivy.set_framework('torch')  # change to any backend!
 model = MyModel()
-
 optimizer = ivy.Adam(1e-4)
-
-x_in = ivy.array([1.,2.,3.])
-
+x_in = ivy.array([1., 2., 3.])
 target = ivy.array([0.])
-
-
 
 def loss_fn(v):
     out = model(x_in, v=v)
-    return ivy. mean((out-target) **2)
-
+    return ivy.torch.reduce_mean((out - target)**2)
 
 for step in range(100):
-    loss,grads = ivy.execute_with_gradients(loss_fn,model.v)
-    model.v = optimizer.step(model.v,grads)
-    print('step{} loss {}'.format(step, ivy.to_numpy(loss).item()))
+    loss, grads = ivy.execute_with_gradients(loss_fn, model.v)
+    model.v = optimizer.step(model.v, grads)
+    print('step {} loss {}'.format(step, ivy.to_numpy(loss).item()))
 
-
-print("Finished training")
-
-
+print('Finished training!')
